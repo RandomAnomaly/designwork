28045ba570988dbf3aff8314f20c76abda946958
jack <jack.gammon@equinox.co.nz>
2016-10-03 15:06:11 +1300

    Sphere drawn on page

--- website/scene.js
+++ website/scene.js
@@ -1,3 +1,7 @@
+/**
+ * Setting the Scene
+ */
+
 var WIDTH = 400, HEIGHT = 300;
 
 // camera
@@ -27,3 +31,47 @@ camera.position.z = 300;
 renderer.setSize(WIDTH, HEIGHT);
 
 $container.append(renderer.domElement);
+
+/**
+ * Adding a mesh
+ */
+
+var sphereMaterial = new THREE.MeshLambertMaterial(
+    {
+        color: 0xD0D3D4
+    }
+);
+
+var radius = 50,
+    segments = 16,
+    rings = 16;
+
+var sphere = new THREE.Mesh(
+    new THREE.SphereGeometry(
+        radius,
+        segments,
+        rings
+    ),
+    sphereMaterial);
+
+scene.add(sphere);
+
+/**
+ * And then there was light!
+ */
+
+var pointLight = new THREE.PointLight(0xFFFFFF);
+
+pointLight.position.x = 10;
+pointLight.position.y = 50;
+pointLight.position.z = 130;
+
+scene.add(pointLight);
+
+
+
+/**
+ * Rendering
+ */
+
+renderer.render(scene, camera);
